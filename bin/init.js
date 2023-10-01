36447c1c3850f303d60f723b59f357933e1e4386
Aleksander Totland <sandberg92@gmail.com>
2023-10-01 13:05:56 +0200

    how bout now?

--- bin/init.js
+++ bin/init.js
@@ -47,7 +47,7 @@ const configPrettier = () => {
 const configGitIgnore = () => {
 	// Add .gitignore
 	// gitignore.io for easy modularity
-	const gitIgnoreConfig = fetch(config.gitIgnoreConfig).then((res) => res.text());
+	const gitIgnoreConfig = fetch(config.gitIgnoreConfig.toString()).then((res) => res.text());
 	// const gitIgnoreConfig = await fetch(
 	// 	'https://www.toptal.com/developers/gitignore/api/windows,linux,macos,visualstudiocode,database,react,reactnative,node'
 	// ).then((res) => res.text());
