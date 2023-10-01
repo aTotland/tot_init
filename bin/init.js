bc85ce23e4b7131e57e9c0b82359f0c6ee45e29f
Aleksander Totland <sandberg92@gmail.com>
2023-10-01 12:24:20 +0200

    - trying direct link

--- bin/init.js
+++ bin/init.js
@@ -47,7 +47,10 @@ const configPrettier = () => {
 const configGitIgnore = async () => {
 	// Add .gitignore
 	// gitignore.io for easy modularity
-	const gitIgnoreConfig = await fetch(config.gitIgnoreConfig.toString()).then((res) => res.text());
+	// const gitIgnoreConfig = await fetch(config.gitIgnoreConfig.toString()).then((res) => res.text());
+	const gitIgnoreConfig = await fetch(
+		'https://www.toptal.com/developers/gitignore/api/windows,linux,macos,visualstudiocode,database,react,reactnative,node'
+	).then((res) => res.text());
 	fse.writeFile('.gitignore', gitIgnoreConfig);
 };
 
