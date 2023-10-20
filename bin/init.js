a373fe1ee3ad1790246283e10af3da6ed2db219f
Aleksander Totland <sandberg92@gmail.com>
2023-10-20 20:58:04 +0200

    made less use of exec sync and instead adding to package.json

--- bin/init.js
+++ bin/init.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const fse = require('fs-extra');
 const path = require('path');
+const fse = require('fs-extra');
 const { execSync } = require('child_process');
 
 const currentDirectory = process.cwd();
@@ -10,13 +10,14 @@ const rootDir = path.join(__dirname, '..');
 const config = fse.readJsonSync(path.join(rootDir, 'config/config.json'));
 const indentRule = { spaces: 2 };
 
+// Revert changes if something errors
 const revertChanges = () => {
-	console.log('Reverting changes!');
+	console.info('Reverting changes!');
 	execSync('git reset --hard && git clean -fd');
 	process.exit(1);
 };
 
-// Check git status for uncommitted changes, in case of an error
+// Check git status for uncommitted changes
 const checkGitStatus = () => {
 	const gitStatus = execSync('git status --porcelain').toString();
 	if (gitStatus.trim() !== '') {
@@ -28,24 +29,6 @@ const checkGitStatus = () => {
 	process.on('SIGINT', revertChanges);
 };
 
-// Create the nodemon configuration
-const configNodemon = () => {
-	const { nodemonConfig } = config;
-	fse.writeJsonSync('nodemon.json', nodemonConfig, indentRule);
-};
-
-// Create the eslint configuration
-const configEslint = () => {
-	const { eslintConfig } = config;
-	fse.writeJsonSync('.eslintrc', eslintConfig, indentRule);
-};
-
-// Create the prettier configuration
-const configPrettier = () => {
-	const { prettierConfig } = config;
-	fse.writeJsonSync('.prettierrc', prettierConfig, indentRule);
-};
-
 const configGitIgnore = async () => {
 	// Add .gitignore
 	// gitignore.io for easy modularity
@@ -55,23 +38,10 @@ const configGitIgnore = async () => {
 	fse.writeFile('.gitignore', gitIgnoreConfig);
 };
 
-const runConfig = async () => {
-	try {
-		console.log(`Initializing ...`);
-		console.log('Setting up configuration files ...');
-		configGitIgnore();
-		configNodemon();
-		configEslint();
-		configPrettier();
-	} catch (error) {
-		throw Error(`Couldn't set up configuration files: ${error}`);
-	}
-};
-
 // Add scripts to package.json
-const addScripts = () => {
+const addConfigs = () => {
 	try {
-		console.log('Updating package.json scripts ...');
+		console.info('Updating package.json scripts & configs ...');
 
 		const packageJson = path.join(currentDirectory, 'package.json');
 
@@ -81,10 +51,16 @@ const addScripts = () => {
 
 		const existingPackageJSON = fse.readJsonSync(packageJson);
 		const { scripts } = config;
+		const { eslintConfig } = config;
+		const { prettier } = config;
+		const { nodemonConfig } = config;
 
 		existingPackageJSON.scripts = {
 			...existingPackageJSON.scripts,
 			...scripts,
+			...eslintConfig,
+			...prettier,
+			...nodemonConfig,
 		};
 
 		fse.writeJsonSync(packageJson, existingPackageJSON, indentRule);
@@ -93,36 +69,53 @@ const addScripts = () => {
 	}
 };
 
-// Install dependencies
-const installDependencies = (dependencyType) => {
-	if (dependencyType === 'dev') {
-		try {
-			console.log('Installing dev dependencies...');
-			execSync(`npm install --save-dev ${config.devDependencies.join(' ')}`);
-		} catch (error) {
-			throw Error(`Could not install dev dependencies.`);
-		}
-	}
+// Add dependencies
+const addDependencies = () => {
 	try {
-		console.log('Installing dependencies ...');
-		execSync(`npm install ${config.dependencies.join(' ')}`);
+		console.log('Updating package.json dependencies ...');
+
+		const packageJson = path.join(currentDirectory, 'package.json');
+
+		if (!fse.existsSync(packageJson)) {
+			throw Error('package.json not found in the current directory.');
+		}
+
+		const existingPackageJSON = fse.readJsonSync(packageJson);
+		const { dependencies } = config;
+		const { devDependencies } = config;
+
+		existingPackageJSON.scripts = {
+			...existingPackageJSON.scripts,
+			...dependencies,
+			...devDependencies,
+		};
+
+		fse.writeJsonSync(packageJson, existingPackageJSON, indentRule);
 	} catch (error) {
-		throw Error(`Could not install dependencies`);
+		throw Error(`Could not update package.json dependencies: ${error}`);
 	}
 };
 
-const runDepInstallers = () => {
-	installDependencies('dev');
-	installDependencies();
-};
-
+// Display success message to user
 const successDisplay = () => {
 	console.group('success!');
-	console.log('It is recommended to add these editor plugins:');
-	console.log('- ESLint');
-	console.log('- Prettier');
+	console.info('It is recommended to add these editor plugins:');
+	console.info('--> ESLint');
+	console.info('--> Prettier');
 };
 
+const runConfig = async () => {
+	try {
+		console.info('Setting up configuration files ...');
+		addConfigs();
+		addDependencies();
+		configGitIgnore();
+	} catch (error) {
+		throw Error(`Couldn't set up configuration files: ${error}`);
+	}
+};
+
+// Display success message to user
 const errorDisplay = (error) => {
 	console.error(`${error.message}`);
 };
@@ -131,8 +124,6 @@ const init = async () => {
 	try {
 		checkGitStatus();
 		runConfig();
-		addScripts();
-		runDepInstallers();
 		successDisplay();
 	} catch (error) {
 		errorDisplay(error);
