3eb94b116b4be56a10bc9402f11ecd18c88ea5a3
Aleksander Totland <sandberg92@gmail.com>
2024-04-09 19:53:46 +0200

    config files

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+module.exports = [
+	{
+		root: true,
+		env: {
+			browser: true,
+			commonjs: true,
+			es2023: true,
+			node: true,
+		},
+		extends: ['eslint:recommended', 'plugin:prettier/recommended', 'prettier'],
+		parserOptions: {
+			ecmaVersion: 'latest',
+		},
+		rules: {
+			'no-console': 0,
+		},
+	},
+];
---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = [
+	{
+		plugins: ['prettier-plugin-ejs'],
+		printWidth: 80,
+		useTabs: true,
+		tabWidth: 2,
+		trailingComma: 'es5',
+		singleAttributePerLine: true,
+		singleQuote: true,
+		bracketSpacing: true,
+		semi: true,
+	},
+];
