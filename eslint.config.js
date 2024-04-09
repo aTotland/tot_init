c6f93ed1a7626d75be98b97a277882ed255ee3b8
Aleksander Totland <sandberg92@gmail.com>
2024-04-09 22:24:38 +0200

    asdasdasd

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,23 @@
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended')
 const eslintConfigPrettier = require('eslint-config-prettier')
-const standard = require('eslint-config-standard')
+// const standard = require('eslint-config-standard')
 
-module.exports = [standard]
+module.exports = [
+  eslintConfigPrettier,
+  // standard,
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: {
+          jsx: true,
+          ejs: true
+        }
+      }
+    },
+    plugins: { eslintPluginPrettierRecommended },
+    rules: {
+      'no-console': 0
+    }
+  }
+]
