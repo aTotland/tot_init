4eb840ffdcc2a8f880eeab734290ae4037d02f3f
Aleksander Totland <sandberg92@gmail.com>
2024-04-09 22:51:25 +0200

    nope no airbnb style

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,10 @@
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended')
 const eslintConfigPrettier = require('eslint-config-prettier')
 // const styleGuide = require('eslint-config-standard')
-const styleGuide = require('eslint-config-airbnb')
 
 module.exports = [
   eslintConfigPrettier,
-  ...[].concat(styleGuide),
+  // ...[].concat(styleGuide)
 
   {
     languageOptions: {
